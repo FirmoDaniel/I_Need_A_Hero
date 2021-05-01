e63d4e3cf1175489cc204c3a81ee1181ba14c042
FirmoDaniel <firmodaniel@gmail.com>
2021-05-01 19:52:39 +0000

    static folder added with style and js files. jquery, materialize and font awesome added to base template.

--- app.py
+++ app.py
@@ -10,6 +10,21 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP","0.0.0.0"),
