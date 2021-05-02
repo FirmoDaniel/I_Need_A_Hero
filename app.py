364d306e7ae9717ce29c192735c232f40fdebf5f
FirmoDaniel <firmodaniel@gmail.com>
2021-05-02 15:48:38 +0000

    added json file for iteration of example characters.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Flask, render_template
 
 
@@ -12,7 +13,10 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    data = []
+    with open("data/characters.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", characters=data)
 
 
 @app.route("/contact")
