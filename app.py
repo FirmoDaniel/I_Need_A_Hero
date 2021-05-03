558a4a5a8aec9e981220836d424bcb862f526f5a
FirmoDaniel <firmodaniel@gmail.com>
2021-05-03 19:30:03 +0000

    UPDATED: script.js with test addClass methods for flash messages.

--- app.py
+++ app.py
@@ -32,6 +32,22 @@ def get_info():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+           flash("Username already exists")
+           return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful")
     return render_template("register.html")
 
 
--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
     
+    let success=$('#test').text();
+    if(success == "Registration Successful"){
+        $('#test').addClass("green")
+    } if(success == "Username already exists"){
+        $('#test').removeClass("green").addClass("red")
+    } 
+
   });
