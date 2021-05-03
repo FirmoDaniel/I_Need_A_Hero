d67af6b8550119315d2ebfe8191ddf5f0b11d3ae
FirmoDaniel <firmodaniel@gmail.com>
2021-05-03 17:48:35 +0000

    ADDED: info.html for trial connection of DBdata which is now working. Mongo, heroku and flask all functional.

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
 import os
 import json
-from flask import Flask, render_template, request, flash
+from flask import (
+    Flask, render_template, request, flash,
+    redirect, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
+# Routing for DB pages
+
+
+@app.route("/get_info")
+def get_info():
+    infos = mongo.db.info.find()
+    return render_template("info.html", infos=infos)
+
 
 # Routing for main pages
 
